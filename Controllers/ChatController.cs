6845996a0d6dde9df182fa25922bdb4bd66b6ac7
Rodrigo Robles <rodri901@hotmail.com>
2019-10-07 21:01:09 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View("Chat");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult SendMessage(string message) {
+            string path = Server.MapPath("~/Content/ChatLog/ChatLog.txt");
+            using (var tw = new StreamWriter(path, true))
+            {
+                //< div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>
+                tw.WriteLine("<div class='msgln'>(" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + "): " + message + "<br></div>");
+            }
+            return Json("succes", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public JsonResult RetrieveMessages()
+        {
+            string[] result = System.IO.File.ReadAllLines(Server.MapPath("~/Content/ChatLog/ChatLog.txt"));
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Scripts/Chat.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    setInterval(loadLog, 2500);
+});
+
+$("#submitmsg").click(function () {
+    var clientmsg = $("#usermsg").val();
+    $.ajax({
+        type: "post",
+        url: "/Chat/SendMessage",
+        data: { "message": clientmsg },
+        success: function () {
+            $("#usermsg").attr("value", "");
+        }
+    });
+});
+
+function loadLog() {
+    $.ajax({
+        url: "/Chat/RetrieveMessages",
+        cache: false,
+        async: false,
+        success: function (e) {
+            $("#chatbox").html(e); //Insert chat log into the #chatbox div	
+
+            //Auto-scroll			
+            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+            if (newscrollHeight > oldscrollHeight) {
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+            }
+        },
+    });
+}
